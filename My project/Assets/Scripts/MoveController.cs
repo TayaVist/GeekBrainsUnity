8e1b26fed8b3d243c23522d19b2e1bf531741501
Taisha <carrotsforlouis@mail.ru>
2022-03-28 20:21:58 +0300

    test push6

---
+++ My project/Assets/Scripts/MoveController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float Speed = 2;
+    [SerializeField]
+    private GameObject Zombie;
+    [SerializeField]
+    private Transform[] points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int index = Random.Range(0, points.Length);
+        var newGO = Instantiate(Zombie, points[index].position, Quaternion.identity);
+        //Destroy(newGO);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            Debug.Log("Space key pressed");
+        }
+
+        var a = Input.GetAxis("Horizontal");
+        var b = Input.GetAxis("Vertical");
+
+        var direction = new Vector3(a, 0, b);
+
+        //transform.position = transform.position + Speed * direction * Time.deltaTime;
+        transform.Translate(Speed * direction * Time.deltaTime);
+
+        Debug.Log(a);
+        Debug.Log(b);
+
+        
+    }
+}
---
+++ My project/Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] Transform a;
+    [SerializeField] Transform b;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(Vector3.Distance(a.position, b.position));
+
+        a.position = Vector3.MoveTowards(a.position, b.position, speed*Time.deltaTime);
+    }
+}
